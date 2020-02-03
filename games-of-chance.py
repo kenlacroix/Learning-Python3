6197248f30820148f58001778eb1df30a0dace2f
Kenneth LaCroix <53909268+kenlacroix@users.noreply.github.com>
2020-02-03 12:31:34 -0800

    Update games-of-chance.py
    Create two_card_draw() and update cho_han().

--- games-of-chance.py
+++ games-of-chance.py
@@ -10,6 +10,7 @@ balance = int(input("How much are you bringing to the table? $"))
 
 #Write your game of chance functions here
 
+#Game: Coin Flip. User calls Heads or Tails
 def coin_flip(guess, bet):
     #Display game banner
     print("_____________________________________")
@@ -37,8 +38,10 @@ def coin_flip(guess, bet):
 
         #Evaluate the results of the flip
         if flip == 1:
+            time.sleep(2)
             print("Flip Results:                           Tails")
         elif flip == 2:
+            time.sleep(2)
             print("Flip Results:                           Heads")
         else:
             print("Something went wrong, please try again")
@@ -52,8 +55,8 @@ def coin_flip(guess, bet):
             loser()
             return -bet
 
+#Game: Cho-Han. Sum two dice rolls. User Chooses Odd or Even.
 def cho_han(guess,bet):
-
     print("Current Balance:                        $" + str(balance))
     print("_____________________________________")
     print("..........Game 2: Cho-Han............")
@@ -77,20 +80,62 @@ def cho_han(guess,bet):
         dice_roll = (die_roll_1 + die_roll_2)
 
         guess = str(input("Choose Even or Odd:                     "))
-
+        print("First die roll:                         " + str(die_roll_1))
+        time.sleep(2)
+        print("Second die roll:                        " + str(die_roll_2))
+        time.sleep(2)
         print("The sum of the dice is:                 " + str(dice_roll))
+        time.sleep(2)
 
         if dice_roll % 2 == 0 and guess == "Even":
             winner()
             return bet
-        elif dice_roll % 2 == 1 and guess == "Even":
-            Winner()
+        elif dice_roll % 2 == 1 and guess == "Odd":
+            winner()
             return bet
         else:
             loser()
             return -bet
-    return 0
 
+def two_card_draw(guess, bet):
+    print("Current Balance:                        $" + str(balance))
+    print("_____________________________________")
+    print(".......Game 3: Two Card Draw.........")
+    print("-------------------------------------")
+    print("")
+
+    print("Rules: Two cards are chosen, the higer one wins ")
+    print("")
+
+    bet = bet()
+
+    your_card = random.randint(1,52)
+    my_card = random.randint(1,52)
+
+    if bet <= 0:
+        print("Your bet must be a positive number")
+        return 0
+    else:
+        print("")
+        print("Your card is:                     " + str(your_card))
+        time.sleep(2)
+        print("My card is:                       " + str(my_card))
+        time.sleep(2)
+        print("")
+
+        #Compare cards, higher one wins
+        if your_card > my_card:
+            winner()
+            return bet
+        elif my_card > your_card:
+            loser()
+            return -bet
+        elif my_card == your_card:
+            print("It was a tie!")
+            return 0
+        else:
+            print("Something went wrong!")
+            return
 def bet():
     bet = int(input("How much are you betting?               $"))
     return bet
@@ -110,3 +155,6 @@ def loser():
 #Call your game of chance functions here
 balance += coin_flip(guess,bet)
 balance += cho_han(guess,bet)
+balance += two_card_draw(guess,bet)
+print("The total game balance is:              $" + str(balance))
+print("")
