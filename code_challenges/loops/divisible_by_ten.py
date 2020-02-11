65e013398214462ba45edb6dc4640981faff030c
Kenneth LaCroix <53909268+kenlacroix@users.noreply.github.com>
2020-02-11 11:36:40 -0800

    Create divisible_by_ten.py

---
+++ code_challenges/loops/divisible_by_ten.py
@@ -0,0 +1,17 @@
+#reate a function named divisible_by_ten() that takes a list of numbers named
+#nums as a parameter.
+#
+#Return the count of how many numbers in the list are divisible by 10.
+
+#Write your function here
+
+def divisible_by_ten(nums):
+  count = 0
+
+  for num in nums:
+    if num % 10 == 0:
+      count += 1
+  return count
+
+#Uncomment the line below when your function is done
+print(divisible_by_ten([20, 25, 30, 35, 40]))
