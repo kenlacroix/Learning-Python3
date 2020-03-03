b699601ece55eaaa3dd1462c17f1ab981264923b
Kenneth LaCroix <53909268+kenlacroix@users.noreply.github.com>
2020-03-03 14:27:07 -0700

    Create count_multi_x.py

---
+++ code_challenges/string_methods/count_multi_x.py
@@ -0,0 +1,12 @@
+# Write your count_multi_char_x function here:
+
+def count_multi_char_x(word, x):
+    counter = (len(word.split(x)) - 1)
+    return(counter)
+
+
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+print(count_multi_char_x("apple", "pp"))
+# should print 1
